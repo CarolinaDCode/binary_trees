da6d5bb25c9be7e672bdb9962d7ae0b2abe8db24
CarolinaDCode <dianacarhuamanta03@gmail.com>
2021-12-02 19:05:03 -0500

    detail

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,6 +9,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 		return;
+	/* pre-order: tree(raiz), left, right */
 	/* call func on the value */
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
