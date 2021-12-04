9744c9c25bb9dc6d29a32795355bd6b43d243253
CarolinaDCode <dianacarhuamanta03@gmail.com>
2021-12-03 22:02:19 -0500

    function that finds the lowest common ancestor of two nodes

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,65 +1,33 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - function that measures the
- * height of a binary tree
- * @tree: pointer to the root node of the tree to measure
- * Return: if tree is null return 0
+ * binary_trees_ancestor - function that checks an ancestor
+ * @first: First node
+ * @second: Second node
+ * Return: the node of the ancestor
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
 {
-	int left_height;
-	int right_height;
+	binary_tree_t *p, *q;
 
-	left_height = 0;
-	right_height = 0;
-	if (!tree)
-		return (0);
-	if (!(tree->right) && !(tree->left))
-		return (0);
-
-	left_height = binary_tree_height(tree->left) + 1;
-	right_height = binary_tree_height(tree->right) + 1;
-
-	if (right_height > left_height)
-		return (right_height);
-	return (left_height);
-}
-
-/**
- * levelorder - recursively perform a function on a binary tree
- * in order to traverse it using level order
- * @tree: tree
- * @i: index of level
- * @func: function to perform
- */
-void levelorder(const binary_tree_t *tree, size_t i, void(*func)(int))
-{
-
-	if (i == 1)
-		func(tree->n);
-	else if (i > 1)
+	if (first == NULL || second == NULL)
 	{
-		levelorder(tree->left, i - 1, func);
-		levelorder(tree->right, i - 1, func);
+		return (NULL);
 	}
-	else
-		return;
-}
+	if (first == second)
+	{
+		return ((binary_tree_t *)first);
+	}
 
-/**
- * binary_tree_levelorder - function goes trough a binary
- * tree using level order traversal
- * @tree: pointer to the root node
- * @func: pointer to a function to call each node
- * Return: nothing
- */
-void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
-{
-	int i, h;
-
-	if (tree == NULL || func == NULL)
-		return;
-	h = binary_tree_height(tree);
-	for (i = 0; i <= h + 1; i++)
-		levelorder(tree, i, func);
+	p = first->parent;
+	q = second->parent;
+	if (p == NULL || first == q || (!p->parent && q))
+	{
+		return (binary_trees_ancestor(first, q));
+	}
+	else if (q == NULL || p == second || (!q->parent && p))
+	{
+		return (binary_trees_ancestor(p, second));
+	}
+	return (binary_trees_ancestor(p, q));
 }
